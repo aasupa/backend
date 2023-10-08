84b72ad55ca53193aa48a5280b434f4fa9ee7927
Aasupa <aasupadbbaral@gmail.com>
2023-10-08 21:26:31 +0545

    orderpage done

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -15,28 +15,35 @@ class Order extends Model
     protected $fillable = [
         
         //'order_number',
-        'user_id',
-        'txn_id',
-        'txn_amount',
-        'payment_status',
-        'order_status',
-        'delivery_address',
+        // 'user_id',
+        // 'txn_id',
+        // 'txn_amount',
+        // 'payment_status',
+        // 'order_status',
+        // 'delivery_address',
 
-        //'order_amount' => 'float',
-       // 'total_tax_amount' => 'float',
-        //'delivery_address_id' => 'integer',
-       // 'delivery_charge' => 'float',
-       // 'user_id' => 'integer',
-       // 'scheduled' => 'integer',
-       // 'details_count' => 'integer',
-       // 'created_at' => 'datetime',
-       // 'updated_at' => 'datetime'
+        'order_amount' => 'float',
+       'total_tax_amount' => 'float',
+        'delivery_address_id' => 'integer',
+       'delivery_charge' => 'float',
+       'user_id' => 'integer',
+       'scheduled' => 'integer',
+       'details_count' => 'integer',
+       'created_at' => 'datetime',
+       'updated_at' => 'datetime'
     ];
+    public function details(){
+        //OrderDetails
+        return $this-> hasMany(OrderDetail::class);
+    }
 
     // public function setDeliveryChargeAttribute($value)
     // {
     //     $this->attributes['delivery_charge'] = round($value, 3);
     // }
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
 
     // public function details()
     // {
